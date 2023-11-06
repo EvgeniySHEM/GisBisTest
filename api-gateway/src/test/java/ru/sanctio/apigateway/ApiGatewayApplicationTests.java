f7ddfdb18f621fcb7b290b2a309ef553871ab40e
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-11-06 22:36:45 +0300

    initial commit

---
+++ api-gateway/src/test/java/ru/sanctio/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sanctio.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
